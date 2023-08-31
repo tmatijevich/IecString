b36ea4ce8e3b2c59c0aa7137f2c958dea1abe303
Tyler Matijevich <tyler.matijevich@br-automation.com>
2023-08-31 16:48:01 -0500

    Update style and conditions in overlap source

--- overlap.c
+++ overlap.c
@@ -1,30 +1,38 @@
 /*******************************************************************************
- * File: IecString\Overlap.c
- * Author: Tyler Matijevich
- * Date: 2022-09-20
+ * File: overlap.c
+ * Created: 2022-09-20
+ * 
+ * Contributors: 
+ * - Tyler Matijevich
+ * 
+ * License:
+ *  This file overlap.c is part of the IecString project released under the
+ *  GNU General Public License v3.0 agreement.  For more information, please 
+ *  visit https://github.com/tmatijevich/IecString/blob/main/LICENSE.
  ******************************************************************************/
 
 #include "main.h"
 
 /* Check if destination overlaps with source */
-uint8_t Overlap(char *Destination, uint32_t Size, char *Source) {
+uint8_t Overlap(char *destination, uint32_t size, char *source) {
 
-	/* Local variables */
-	uint32_t Length;
-	
-	/* Verify parameters */
-	if(Destination == NULL || Source == NULL)
-		return false;
-		
-	/* Does destination's start overlap source's string length including null terminator */
-	Length = strlen(Source);
-	if(Source <= Destination && Destination <= Source + Length)
-		return true;
-		
-	/* Does destination's end overlap source's string length */
-	if(Source <= Destination + Size - 1 && Destination + Size - 1 <= Source + Length)
-		return true;
-		
-	/* No overlap */
-	return false;
+    /* Local variables */
+    uint8_t overlap, end;
+    uint32_t length;
+    
+    /* Verify parameters */
+    if (destination == NULL || source == NULL)
+        return false;
+        
+    length = strlen(source);
+    
+    /* Does destination's start overlap source's length */
+    overlap = source <= destination && destination <= source + length;
+
+    /* Does destination's end overlap source's length */
+    end = destination + size - 1;
+    overlap |= source <= end && end <= source + length;
+    
+    /* No overlap */
+    return overlap ? true : false;
 }
